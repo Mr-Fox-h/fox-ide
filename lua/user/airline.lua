a60d7712633aaa2f2b8354fbe34835585f917e65
mr.fox <mr.fox@iran.ir>
2023-06-05 18:24:32 +0330

    13 Commit

--- lua/user/airline.lua
+++ lua/user/airline.lua
@@ -36,16 +36,18 @@ local conditions = {
 -- Config
 local config = {
   options = {
+    theme = 'kanagawa',
     -- Disable sections and component separators
     component_separators = '',
-    section_separators = '',
-    theme = {
+    section_separators = { left = '', right = '' },
+    --section_separators = '',
+    --theme = {
       -- We are going to use lualine_c an lualine_x as left and
       -- right section. Both are highlighted by c theme .  So we
       -- are just setting default looks o statusline
-      normal = { c = { fg = colors.fg, bg = colors.bg } },
-      inactive = { c = { fg = colors.fg, bg = colors.bg } },
-    },
+    --  normal = { c = { fg = colors.fg, bg = colors.bg } },
+    --  inactive = { c = { fg = colors.fg, bg = colors.bg } },
+    --},
   },
   sections = {
     -- these are to remove the defaults
@@ -80,7 +82,7 @@ end
 
 ins_left {
   function()
-    return '▊'
+    return ''
   end,
   color = { fg = colors.orange }, -- Sets highlighting of component
   padding = { left = 0, right = 1 }, -- We don't need space before this
@@ -116,8 +118,7 @@ ins_left {
       t = colors.red,
     }
     return { fg = mode_color[vim.fn.mode()] }
-  end,
-  padding = { right = 1 },
+  end
 }
 
 ins_left {
@@ -129,6 +130,7 @@ ins_left {
 ins_left {
   'filename',
   cond = conditions.buffer_not_empty,
+  icon = '󰈔',
   color = { fg = colors.magenta, gui = 'bold' },
 }
 
@@ -172,7 +174,7 @@ ins_left {
     end
     return msg
   end,
-  icon = ' LSP:',
+  icon = '  [LSP] ',
   color = { fg = '#ffffff', gui = 'bold' },
 }
 
@@ -211,7 +213,7 @@ ins_right {
 
 ins_right {
   function()
-    return '▊'
+    return ''
   end,
   color = { fg = colors.orange },
   padding = { left = 1 },
--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,28 @@
-local colorscheme = "kanagawa-dragon"
+-- Default options:
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
+})
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
-
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa-dragon")
