9eaf81e5b83078a27b3d004fa6c72dd7ad430ae1
mr-fox <mr-fox@mr-fox.mr-fox>
2023-08-25 22:37:55 -1100

    27 Commit

--- lua/user/airline.lua
+++ lua/user/airline.lua
@@ -18,19 +18,19 @@ local colors = {
 require('lualine').setup {
   options = {
     theme = 'auto',
-    component_separators = '|',
+    component_separators = '▎',
     section_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', icon = '', separator = { left = '', right = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
+    lualine_b = { 'filename', {'branch', icon = ''} },
     lualine_c = { 'fileformat' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      { 'location', separator = { right = '', left = '' }, left_padding = 2 },
     },
   },
   inactive_sections = {
