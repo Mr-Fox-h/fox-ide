e771577027eb61d5e2317bb77b35047a568ac996
Mr-Fox-h <mr.fox@iran.ir>
2024-12-09 20:57:02 +0330

    Edit: Default color theme.

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,7 +1,7 @@
 --"rebelot/kanagawa.nvim"
 --"folke/tokyonight.nvim"
 function ColorsTheme(color)
-  color = color or "kanagawa-dragon"
+  color = color or "kanagawa"
   vim.cmd.colorscheme(color)
 end
 ColorsTheme()
