c4e2f4a638e9d88bb70e429a7ab1a70c2dae43f2
Mr-Fox-h <mr.fox@iran.ir>
2024-12-10 13:20:02 +0330

    Fixed: LSP formatting

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -81,7 +81,6 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- LSP Formating --
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
-    local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if not client then return end
     if client.supports_method('textDocument/formatting') then
