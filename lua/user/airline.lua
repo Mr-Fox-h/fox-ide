4dde130051dbd4d431a2730785ad7f15520b26a1
Mr-Fox-h <mr.fox@iran.ir>
2024-12-10 12:24:32 +0330

    Fixed: Clean code.

--- lua/user/airline.lua
+++ lua/user/airline.lua
@@ -2,8 +2,8 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -18,18 +18,18 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {{ 'mode', icon = '' }},
-    lualine_b = {{ 'branch', icon = '' }, 'diff', 'filetype', { 'filename', icon = '鑢' }},
+    lualine_a = { { 'mode', icon = '' } },
+    lualine_b = { { 'branch', icon = '' }, 'diff', 'filetype', { 'filename', icon = '鑢' } },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {{'diagnostics', sources = { 'nvim_diagnostic' }, symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰌵' }}},
-    lualine_z = {{ 'fileformat', symbols = { unix = '', dos = '', mac = '' }, separator = { right = '', left = '' }, color = {fg = nil, bg = '#f2545b'}}, 'filesize', { 'location', separator = { right = '', left = '' }}, {'progress', icon = '', separator = { right = '▊', left = '' }, color = {bg = '#74c69d'}}}
+    lualine_y = { { 'diagnostics', sources = { 'nvim_diagnostic' }, symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰌵' } } },
+    lualine_z = { { 'fileformat', symbols = { unix = '', dos = '', mac = '' }, separator = { right = '', left = '' }, color = { fg = nil, bg = '#f2545b' } }, 'filesize', { 'location', separator = { right = '', left = '' } }, { 'progress', icon = '', separator = { right = '▊', left = '' }, color = { bg = '#74c69d' } } }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
